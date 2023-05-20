d2059e0c49c09c5cc9126e1ee2eef519b9d07ab3
Herroa <roman.gordov@mail.ru>
2023-05-20 21:05:31 +0700

    Created information message

--- src/pwgen/main.c
+++ src/pwgen/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 
 int main(int argc, char *argv[])
 {
-    printf("Hello world");
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-h") == 0)
+        {
+            printf("./pwgen [ OPTION ] [ pw_length ] [ num_pw ]\nExample:./pwgen -A 6 4\n-0 В генерируемом пароле не будет цифр\n-А В генерируемом пароле не будет заглавных букв\n-N В генерируемом пароле не будет букв\n-n В генерируемом пароле будет как минимум одна цифра\n-y В генерируемом пароле будет минимум один спец. символ\n");
+            return 0;
+        }
+    }
     return 0;
 }
