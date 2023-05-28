26949de885c063b2d66a1629c23e653b3af0c438
Herroa <roman.gordov@mail.ru>
2023-05-28 13:23:31 +0700

    Code style changes

--- src/pwgen/main.c
+++ src/pwgen/main.c
@@ -1,34 +1,37 @@
-#include <stdio.h>
-#include <time.h>
-#include <string.h>
 #include <libpwgen/argument_processing.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
 
-
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     srand(time(NULL));
-    for (int i = 1; i < argc; i++)
-    {
-        if (strcmp(argv[i], "-h") == 0)
-        {
-            printf("./pwgen [ OPTION ] [ pw_length ] [ num_pw ]\nExample:./pwgen -A 6 4\n-0 В генерируемом пароле не будет цифр\n-А В генерируемом пароле не будет заглавных букв\n-N В генерируемом пароле не будет букв\n-n В генерируемом пароле будет как минимум одна цифра\n-y В генерируемом пароле будет минимум один спец. символ\n");
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-h") == 0) {
+            printf("./pwgen [ OPTION ] [ pw_length ] [ num_pw "
+                   "]\nExample:./pwgen -A 6 4\n-0 В генерируемом пароле не "
+                   "будет цифр\n-А В генерируемом пароле не будет заглавных "
+                   "букв\n-N В генерируемом пароле не будет букв\n-n В "
+                   "генерируемом пароле будет как минимум одна цифра\n-y В "
+                   "генерируемом пароле будет минимум один спец. символ\n");
             return 0;
         }
     }
-    if (argc < 3)
-    {
-        printf("Not enough arguments!\nTo know more about arguments, type 'pwgen -h'\n");
+    if (argc < 3) {
+        printf("Not enough arguments!\nTo know more about arguments, type "
+               "'pwgen -h'\n");
         return 0;
     }
-    if (argc > 7)
-    {
-        printf("Too many arguments!\nTo know more about arguments, type 'pwgen -h'\n");
+    if (argc > 7) {
+        printf("Too many arguments!\nTo know more about arguments, type 'pwgen "
+               "-h'\n");
         return 0;
     }
 
-    int len = 0; int quantity = 0;
+    int len = 0;
+    int quantity = 0;
     check_len_quantity(argv, argc, &len, &quantity);
-    char *pool = pool_assembling(argv, argc);
+    char* pool = pool_assembling(argv, argc);
     generator(pool, len, quantity);
 
     free(pool);
