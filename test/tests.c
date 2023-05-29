cd8485a59cfb1abaff7fb19667778eda646e9abd
Herroa <roman.gordov@mail.ru>
2023-05-29 13:24:19 +0700

    Test check_len_quantity function

--- test/tests.c
+++ test/tests.c
@@ -17,19 +17,67 @@
 // }
 
 
-CTEST(pwgen, pool_assembling)
+// CTEST(argument_processing, pool_assembling)
+// {
+//     // Given
+//     char *argv[] = {"./bin/pwgen","10","2","-y", "-N", "-A", "-0"};
+//     int argc = 7;
+//     // char *first = "-y";
+//     // char *second = "-N";
+//     // char *third = "-A";
+//     // char *forth = "-0";
+    
+//     // When
+//     char *result = pool_assembling(argv,argc);
+//     // Then
+//     char symbols[] = "!#$&'()*+,/:;<=>?@[]^_{|}~`";
+//     ASSERT_EQUAL(symbols, result);
+// }
+
+CTEST(argument_processing, check_len_quantity_1)
 {
     // Given
-    char **argv = {"./bin/pwgen","10","2","-y", "-N", "-A", "-0"};
+    char *argv[] = {"./bin/pwgen","10","2","-y", "-N", "-A", "-0"};
     int argc = 7;
-    // char *first = "-y";
-    // char *second = "-N";
-    // char *third = "-A";
-    // char *forth = "-0";
-    
+    int len = 0; int quantity = 0;
     // When
-    const char *result = pool_assembling(argv,argc);
+    check_len_quantity(argv,argc,&len,&quantity);
+
     // Then
-    char symbols[] = "!#$&'()*+,/:;<=>?@[]^_{|}~`";
-    ASSERT_EQUAL(symbols, result);
+    int expected_len = 10;
+    int expected_quan = 2;
+    ASSERT_EQUAL(expected_len, len);
+    ASSERT_EQUAL(expected_quan, quantity);
+}
+
+CTEST(argument_processing, check_len_quantity_2)
+{
+    // Given
+    char *argv[] = {"./bin/pwgen","5","-0","-y", "-N", "-A", "20"};
+    int argc = 7;
+    int len = 0; int quantity = 0;
+    // When
+    check_len_quantity(argv,argc,&len,&quantity);
+
+    // Then
+    int expected_len = 5;
+    int expected_quan = 20;
+    ASSERT_EQUAL(expected_len, len);
+    ASSERT_EQUAL(expected_quan, quantity);
+}
+
+CTEST(argument_processing, check_len_quantity_3)
+{
+    // Given
+    char *argv[] = {"./bin/pwgen","-y","-0","100", "-N", "1", "-N"};
+    int argc = 7;
+    int len = 0; int quantity = 0;
+    // When
+    check_len_quantity(argv,argc,&len,&quantity);
+
+    // Then
+    int expected_len = 100;
+    int expected_quan = 1;
+    ASSERT_EQUAL(expected_len, len);
+    ASSERT_EQUAL(expected_quan, quantity);
 }
